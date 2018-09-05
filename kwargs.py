b6298b72d4ee9ac223f62e6266d75c54927305a0
Franz Noel Tanglao <franz@tanglao.org>
2018-09-05 12:24:21 -0700

    feat: Print kwargs

--- kwargs.py
+++ kwargs.py
@@ -1,5 +1,10 @@
 def testing_kwargs(**kwargs):
+    # print the dictionary
+    print(kwargs)
+
+    # print each kwarg items
     for key, value in kwargs.items():
         print("{0} {1}".format(key,value))
 
 testing_kwargs(firstname="John",lastname="Smith",age="45")
+
