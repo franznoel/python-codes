121e7b8a379f148579c83a489dc53f84b6c7ebb4
Franz Noel Tanglao <franz@tanglao.org>
2018-09-05 13:13:23 -0700

    feat: Add loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+
+# count from zero to 10
+for i in range(0,10):
+    print (i)
+
+print("")
+
+# count by 2
+for i in range(0,10,2):
+    print(i)
+
+print("")
+
+# count by 5 start at 50 to 100
+for i in range(50,100,5):
+    print(i)
